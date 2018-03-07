af999a595407f59f33a6c23dd9db7301a41f2159
Dane Liergaard <dzlier@amazon.com>
2018-03-07 09:18:32 -0800

    Adding DLP - Inspect string & file samples.

---
+++ dlp/api/DlpSample/Program.cs
@@ -0,0 +1,159 @@
+﻿// Copyright 2018 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using CommandLine;
+using Google.Cloud.Dlp.V2;
+using Google.Protobuf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace GoogleCloudSamples
+{
+    abstract class InspectLocalOptions
+    {
+        [Value(0, HelpText = "The project ID to run the API call under.", Required = true)]
+        public string ProjectId { get; set; }
+        [Option('i', "info-types", HelpText = "Comma-separated infoTypes of information to match.",
+            Default = "PHONE_NUMBER,EMAIL_ADDRESS,CREDIT_CARD_NUMBER")]
+        public string InfoTypes { get; set; }
+        [Option('l', "minimum-likelihood",
+            HelpText = "The minimum likelihood required before returning a match (0-5).", Default = 0)]
+        public int MinLikelihood { get; set; }
+        [Option('m', "max-findings",
+            HelpText = "The maximum number of findings to report per request (0 = server maximum).", Default = 0)]
+        public int MaxFindings { get; set; }
+        [Option('n', "no-includeQuotes", HelpText = "Do not include matching quotes.")]
+        public bool NoIncludeQuote { get; set; }
+    }
+
+    [Verb("inspectString", HelpText = "Inspects a content string.")]
+    class InspectStringOptions : InspectLocalOptions
+    {
+        [Value(1, HelpText = "The item to inspect.", Required = true)]
+        public string Value { get; set; }
+    }
+
+    [Verb("inspectFile", HelpText = "Inspects a content file.")]
+    class InspectFileOptions : InspectLocalOptions
+    {
+        [Value(1, HelpText = "The path to the local file to inspect. Can be a text, JPG, or PNG.", Required = true)]
+        public string File { get; set; }
+    }
+
+    public class Dlp
+    {
+        static IEnumerable<InfoType> ParseInfoTypes(string infoTypesStr)
+        {
+            return infoTypesStr.Split(',').Select(str =>
+            {
+                try
+                {
+                    return InfoType.Parser.ParseJson($"{{\"name\": \"{str}\"}}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Failed to parse infoType {str}: {e}");
+                    return null;
+                }
+            }).Where(it => it != null);
+        }
+
+        static object InspectString(InspectStringOptions opts)
+        {
+            return InspectLocal(opts, new InspectContentRequest
+            {
+                Parent = $"projects/{opts.ProjectId}",
+                Item = new ContentItem
+                {
+                    Value = opts.Value
+                }
+            });
+        }
+
+        static readonly Dictionary<string, ByteContentItem.Types.BytesType> fileTypes =
+            new Dictionary<string, ByteContentItem.Types.BytesType>()
+        {
+            {"bmp", ByteContentItem.Types.BytesType.ImageBmp},
+            {"jpg", ByteContentItem.Types.BytesType.ImageJpeg},
+            {"jpeg", ByteContentItem.Types.BytesType.ImageJpeg},
+            {"png", ByteContentItem.Types.BytesType.ImagePng},
+            {"svg", ByteContentItem.Types.BytesType.ImageSvg},
+            {"txt", ByteContentItem.Types.BytesType.TextUtf8}
+        };
+
+        static object InspectFile(InspectFileOptions opts)
+        {
+            var fileStream = new FileStream(opts.File, FileMode.Open);
+            try
+            {
+                return InspectLocal(opts, new InspectContentRequest
+                {
+                    Parent = $"projects/{opts.ProjectId}",
+                    Item = new ContentItem
+                    {
+                        ByteItem = new ByteContentItem
+                        {
+                            Data = ByteString.FromStream(fileStream),
+                            Type = fileTypes.GetValueOrDefault(new FileInfo(opts.File).Extension.ToLower(),
+                                    ByteContentItem.Types.BytesType.Unspecified)
+                        }
+                    }
+                });
+            } finally
+            {
+                fileStream.Close();
+            }
+        }
+
+        private static object InspectLocal(InspectLocalOptions opts,
+            InspectContentRequest request)
+        {
+            var inspectConfig = new InspectConfig
+            {
+                MinLikelihood = (Likelihood)opts.MinLikelihood,
+                Limits = new InspectConfig.Types.FindingLimits
+                {
+                    MaxFindingsPerRequest = opts.MaxFindings
+                },
+                IncludeQuote = !opts.NoIncludeQuote
+            };
+            inspectConfig.InfoTypes.AddRange(ParseInfoTypes(opts.InfoTypes));
+            request.InspectConfig = inspectConfig;
+            DlpServiceClient dlp = DlpServiceClient.Create();
+            InspectContentResponse response = dlp.InspectContent(request);
+            var count = 0;
+            var findingsStr = "";
+            foreach (var finding in response.Result.Findings)
+            {
+                findingsStr += $"\nFinding {count++}: \n\t{finding}";
+            }
+            var wereOrNotTruncated = "were" + (response.Result.FindingsTruncated ? "" : " not") + " truncated";
+            Console.WriteLine($"Found {count} results, and results {wereOrNotTruncated}: {findingsStr}");
+            return 0;
+        }
+
+        public static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<InspectStringOptions, InspectFileOptions>(args)
+                .MapResult(
+                (InspectStringOptions opts) => InspectString(opts),
+                (InspectFileOptions opts) => {
+                    Console.WriteLine($"projectID: {opts.ProjectId}\nfile: {opts.File}\ninfoTypes: {opts.InfoTypes}\nlikelihood: {opts.MinLikelihood}\nmaxResults: {opts.MaxFindings}");
+                    return InspectFile(opts); },
+                errs => 1);
+        }
+    }
+}
---
+++ dlp/api/DlpTest/DlpTest.cs
@@ -0,0 +1,123 @@
+﻿// Copyright 2018 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Xunit;
+
+namespace GoogleCloudSamples
+{
+    public class DlpTest
+    {
+        private readonly string _projectId;
+        const string phone = "(223) 456-7890";
+        const string email = "gary@somedomain.org";
+        const string cc = "4000-3000-2000-1000";
+        const string inspectFilePath = "test.txt";
+        const string inspectStringValue = "hi my phone number is (223) 456-7890 and my email address is gary@somedomain.org. " +
+            "You'll find my credit card number is 4000-3000-2000-1000!";
+
+        readonly CommandLineRunner _dlp = new CommandLineRunner()
+        {
+            VoidMain = Dlp.Main,
+            Command = "Dlp"
+        };
+
+        protected ConsoleOutput Run(params string[] args)
+        {
+            string cmd = args[0].ToLower();
+            return _dlp.Run(args);
+        }
+
+        public DlpTest()
+        {
+            _projectId = Environment.GetEnvironmentVariable("GOOGLE_PROJECT_ID");
+        }
+
+        private void AssertPhoneEmailCC(ConsoleOutput output, bool checkPhone, bool checkEmail, bool checkCc)
+        {
+            var results = 0;
+            if (checkPhone) results++;
+            if (checkEmail) results++;
+            if (checkCc) results++;
+            Assert.Contains($"Found {results} results", output.Stdout);
+            if (checkPhone) Assert.Contains(phone, output.Stdout);
+            if (checkEmail) Assert.Contains(email, output.Stdout);
+            if (checkCc) Assert.Contains(cc, output.Stdout);
+        }
+
+        /// <summary>
+        /// With default params, Dlp will find matches of all info types and likelihoods, including quote info.
+        /// </summary>
+        [Theory]
+        [InlineData("inspectString", inspectStringValue)]
+        [InlineData("inspectFile", inspectFilePath)]
+        public void TestInspectStringFilterInfoTypes(string verb, string value)
+        {
+            AssertPhoneEmailCC(Run(verb, _projectId, value), true, true, true);
+            AssertPhoneEmailCC(Run(verb, _projectId, value, "-i", "PHONE_NUMBER,EMAIL_ADDRESS,CREDIT_CARD_NUMBER"), true, true, true);
+            AssertPhoneEmailCC(Run(verb, _projectId, value, "-i", "PHONE_NUMBER,EMAIL_ADDRESS"), true, true, false);
+            AssertPhoneEmailCC(Run(verb, _projectId, value, "-i", "PHONE_NUMBER,CREDIT_CARD_NUMBER"), true, false, true);
+            AssertPhoneEmailCC(Run(verb, _projectId, value, "-i", "EMAIL_ADDRESS,CREDIT_CARD_NUMBER"), false, true, true);
+            AssertPhoneEmailCC(Run(verb, _projectId, value, "-i", "PHONE_NUMBER"), true, false, false);
+            AssertPhoneEmailCC(Run(verb, _projectId, value, "-i", "EMAIL_ADDRESS"), false, true, false);
+            AssertPhoneEmailCC(Run(verb, _projectId, value, "-i", "CREDIT_CARD_NUMBER"), false, false, true);
+        }
+
+        /// <summary>
+        /// Dlp evaluates this content string as having 
+        /// PHONE_NUMBER - VERY_LIKELY
+        /// EMAIL_ADDRESS - LIKELY
+        /// CREDIT_CARD_NUMBER - VERY_LIKELY
+        /// </summary>
+        [Theory]
+        [InlineData("inspectString", inspectStringValue)]
+        [InlineData("inspectFile", inspectFilePath)]
+        public void TestInspectStringFilterLikelihood(string verb, string value)
+        {
+            AssertPhoneEmailCC(Run(verb, _projectId, value, "-l", "4"), true, true, true);
+            AssertPhoneEmailCC(Run(verb, _projectId, value, "-l", "5"), true, false, true);
+        }
+
+        /// <summary>
+        /// Dlp finds the matches in the order phone -> email -> credit card
+        /// </summary>
+        [Theory]
+        [InlineData("inspectString", inspectStringValue)]
+        [InlineData("inspectFile", inspectFilePath)]
+        public void TestInspectStringFilterMaxResults(string verb, string value)
+        {
+            AssertPhoneEmailCC(Run(verb, _projectId, value, "-m", "0"), true, true, true);
+            AssertPhoneEmailCC(Run(verb, _projectId, value, "-m", "3"), true, true, true);
+            AssertPhoneEmailCC(Run(verb, _projectId, value, "-m", "2"), true, true, false);
+            AssertPhoneEmailCC(Run(verb, _projectId, value, "-m", "1"), true, false, false);
+        }
+    }
+
+
+    //public class QuickStartTests
+    //{
+    //    readonly CommandLineRunner _quickStart = new CommandLineRunner()
+    //    {
+    //        VoidMain = QuickStart.Main,
+    //        Command = "dotnet run"
+    //    };
+
+    //    [Fact]
+    //    public void TestRun()
+    //    {
+    //        var output = _quickStart.Run();
+    //        Assert.Equal(0, output.ExitCode);
+    //    }
+    //}
+}
